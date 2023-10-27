84ffd46bdd4df2378074226920af354fc271a8ab
Rajath E S <rajathes999@gmail.com>
2023-10-27 18:44:48 +0530

    Set debug to False

--- sample_api/sample_api/settings.py
+++ sample_api/sample_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!f(fqn9h2b68)ib4*8e_6%w(!$@639wjhxe9d)p&uf=*$4c)q!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'appserver']
 
